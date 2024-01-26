64d1e71a1e7c4f17fd6b0b8eac1e1190c40dac31
Egg Long <eddres229@gmail.com>
2024-01-26 11:25:09 -0500

    Initial Commit

---
+++ EventLM/EventLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventLMApp.swift
+//  EventLM
+//
+//  Created by Eli Dresnin on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct EventLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
